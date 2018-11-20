acf3c86cc333cb5dcaa0ae8e3e3791320112cf9d
Koppa <t@ioover.net>
2018-11-20 14:27:17 +0800

    dot retrieve deleted item

--- src/models/get_item_list.sql
+++ src/models/get_item_list.sql
@@ -1,7 +1,8 @@
 SELECT
-  L.id, R.id, L.content, L.fold, L.metadata, L.deleted,
+  L.id, R.id, L.content, L.fold, L.metadata,
   L.favorite, L.tags, L.created, L.modified
 FROM
   items L LEFT JOIN items R
   ON subpath(L.path, 0, -1) = R.path
+WHERE NOT L.deleted
 ORDER BY (R.id, L.path);
